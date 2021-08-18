97d4c6b87829bbb3d8940f9763c96e5e565c8403
Xinshuoo <huangxinshuo@live.com>
2021-08-18 15:29:19 +0800

    Edited index.html, script.js, style.css

--- script.js
+++ script.js
@@ -1,8 +1,39 @@
-const container = document.querySelector("#container");
+const container = document.querySelector(".container");
 
-const div = document.createElement("div");
+createGrid();
 
-for (let i = 0; i < 16; i++) {
-    div.classList.add(`div${i + 1}`);
-    container.appendChild(div)
+const clearGridButton = document.querySelector("#clear");
+
+clearGridButton.addEventListener("click", clearGrid)
+
+function createGrid (sideLength) {
+    if (!(sideLength > 0)) { // check that a valid side length is given
+        sideLength = 16;
+    } else if (sideLength > 100) { // set anything >100 to 100
+        sideLength = 100;
+    }
+
+    container.style.gridTemplateColumns = `repeat(${sideLength}, auto)`;
+    for (let i = 0; i < (sideLength ** 2); i++) {
+        const div = document.createElement("div");
+        div.classList.add("item");
+        div.id = `div${i + 1}`;
+        container.appendChild(div);
+    }
+
+    const items = document.querySelectorAll(".item")
+
+    items.forEach(element => {
+        element.addEventListener("mouseover", changeColor)
+    });
 }
+
+function changeColor(e) {
+    this.style.backgroundColor = "black";
+}
+
+function clearGrid() {
+    container.innerHTML = "";
+    createGrid(+prompt("How many squares per side? (max 100)"));
+}
+
